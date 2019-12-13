8cb8bb757b8ae2b231932b090f27893ba5998647
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-13 16:36:58 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Comment extends Component {
   render() {
     return (
+<<<<<<< HEAD
       <div className="comment">
+=======
+      <div>
+>>>>>>> f9333f678fec3c9a01a737fd230469414d5fd32c
       {this.props.commentText}
       </div>
       
